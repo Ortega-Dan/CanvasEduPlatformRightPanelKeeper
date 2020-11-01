a6cf23b9978ac8b1422d2448405a83e69aa19d68
Dan Ortega <ortega.dan2010@gmail.com>
2020-10-31 23:42:32 -0500

    Create shortScriptForDeveloperToolsConsole.js

---
+++ shortScriptForDeveloperToolsConsole.js
@@ -0,0 +1,5 @@
+document.getElementById('main').style.width = '58vw'
+var rightWrap = document.getElementById('right-side-wrapper')
+rightWrap.style.position = 'fixed'
+rightWrap.style.right = '10px'
+rightWrap.style.top = '-10px'
